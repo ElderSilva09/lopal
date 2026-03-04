eaa73541c1426c3f6a014d40028af120c51ecc55
ElderSilva09 <eldersilvaeps2009@gmail.com>
2026-03-04 16:53:13 -0300

    Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -4,6 +4,8 @@ function calculadora(){
         let operando1;
         let operando2;
         let operador;
+        let continua;
+        let operador_valido = true;
 
         operando1 = parseFloat( prompt("Digite o primeiro número: ") );
         operando2 = parseFloat( prompt("Digite o segundo número: ") );
@@ -17,16 +19,28 @@ function calculadora(){
             resultado = operando1 * operando2;
         }  else if (operador === "/" ){
             if (operando2 == 0){
-                alert("Não pode dividir por 0😢")
+                alert("Não pode dividir por 0😢");
+                continua = prompt ("Digite sim para continuar ou não para encerrar.");
+                if(continua === "não"){
+                    return
+                }
+            } else {  
+                resultado = operando1 / operando2;                         
             }
-            resultado = operando1 / operando2;
+
         } else {
             //escreva algo😎 
-            alert("Algo de errado não está certo🤔")
-            return
+            alert("Algo de errado não está certo🤔");
+            operador_valido = false;
+            continua = prompt("Digite sim para continuar ou não para encerrar");
+            if(continua === "não"){
+                return
+            }
         }
+        if(operador != "/" || operando2 != 0){
         alert("Resultado: " + 
             operando1 + " " + operador + " " + operando2 + " = " + resultado);
+        }
     }
 }       
  
---
+++ calculadora/meuscript.js
@@ -0,0 +1,16 @@
+function minhaFunc(){
+    let variavel1 = 0;
+    let variavel2 = 0;
+    
+    while(true){
+        if(true){
+
+        } else {
+            if(felse){
+              if( true){
+                    alert ("uma string extremamente comprida pra caramba")
+              }  
+            }
+        }
+    }
+}
